4e1af14510cbe46348babd0c496f3feccae4183f
capricorn <8250820+capricorn@users.noreply.github.com>
2024-03-22 21:14:08 -0600

    Implement timer task for displaying elapsed time

--- Footprints/Logger/LoggerView.swift
+++ Footprints/Logger/LoggerView.swift
@@ -11,16 +11,28 @@ struct LoggerView: View {
     @StateObject var model: LoggerViewModel
     
     var body: some View {
-        VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            Spacer()
-            Image(systemName: "record.circle")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 64, height: 64)
-                .foregroundColor(model.recordButtonForegroundColor)
-                .onTapGesture(perform: model.record)
-                .animation(.easeInOut, value: model.recording)
+        ZStack {
+            VStack {
+                Group {
+                    if model.recording {
+                        Text("\(model.elapsedLogTime ?? 0)")
+                    } else {
+                        Text(Date.now.formatted(.dateTime))
+                    }
+                }
+                .font(.system(size: 32))
+            }
+            VStack {
+                // TODO: At 1/3 boundary
+                Spacer()
+                Image(systemName: "record.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 64, height: 64)
+                    .foregroundColor(model.recordButtonForegroundColor)
+                    .onTapGesture(perform: model.record)
+                    .animation(.easeInOut, value: model.recording)
+            }
         }
     }
 }
--- Footprints/Logger/LoggerViewModel.swift
+++ Footprints/Logger/LoggerViewModel.swift
@@ -10,6 +10,9 @@ import SwiftUI
 
 class LoggerViewModel: ObservableObject {
     @Published var logStartDate: Date?
+    @Published var logNowDate: Date?
+    
+    private var timerTask: Task<Void, Never>?
     
     /// `true` if a session is currently being recorded.
     var recording: Bool {
@@ -20,7 +23,28 @@ class LoggerViewModel: ObservableObject {
         recording ? .red : .black
     }
     
+    /// Total run time of the logger.
+    var elapsedLogTime: TimeInterval? {
+        guard let logStartDate, let logNowDate else {
+            return nil
+        }
+        
+        return logNowDate.timeIntervalSince(logStartDate)
+    }
+    
     func record() {
-        logStartDate = recording ? nil : .now
+        if recording {
+            timerTask?.cancel()
+            timerTask = nil
+            logStartDate = nil
+        } else {
+            timerTask = Task.detached {
+                while Task.isCancelled == false {
+                    self.logNowDate = Date.now
+                    try? await Task.sleep(nanoseconds: UInt64(1e9/60))
+                }
+            }
+            logStartDate = Date.now
+        }
     }
 }
