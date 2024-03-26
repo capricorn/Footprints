fdaf43f162830c873ac191e55559a62c6b1cc0bb
capricorn <8250820+capricorn@users.noreply.github.com>
2024-03-25 20:43:48 -0600

    Create Session sql model

---
+++ Footprints/Core/Sqlite/Models/Session.swift
@@ -0,0 +1,15 @@
+//
+//  Session.swift
+//  Footprints
+//
+//  Created by Collin Palmer on 3/25/24.
+//
+
+import Foundation
+import GRDB
+
+struct Session: Identifiable, Codable, FetchableRecord, PersistableRecord {
+    let id: UUID
+    let startTimestamp: Float
+    let endTimestamp: Float
+}
