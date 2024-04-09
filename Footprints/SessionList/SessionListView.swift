a8e50e6bc618f5bf05f37c2a937f1bdefd7a5414
capricorn <8250820+capricorn@users.noreply.github.com>
2024-04-09 15:34:28 -0600

    Connect session list view to db

--- Footprints/SessionList/SessionListView.swift
+++ Footprints/SessionList/SessionListView.swift
@@ -7,15 +7,59 @@
 
 import SwiftUI
 import GRDB
+import Combine
 
 struct SessionListView: View {
     @Environment(\.databaseQueue) var dbQueue: DatabaseQueue
+    @State var sessions: [SessionModel] = []
+    @State private var sessionSubscriber: AnyDatabaseCancellable?
     
     var body: some View {
-        Text("")
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Sessions")
+                    .font(.title)
+                ForEach(sessions) { session in
+                    SessionListItemView(sessionItem: session)
+                        .padding()
+                }
+                Spacer()
+            }
+        }
+        .onAppear {
+            // TODO: Move to vm
+            let sessionObserver = ValueObservation.tracking { db in
+                try! SessionModel.fetchAll(db)
+            }
+            
+            sessionSubscriber = sessionObserver.start(in: dbQueue, onError: { error in
+                print("DB error: \(error)")
+            }, onChange: { sessions in
+                self.sessions = sessions
+            })
+        }
+    }
+}
+
+private struct PreviewView: View {
+    @Environment(\.databaseQueue) var dbQueue: DatabaseQueue
+    
+    var body: some View {
+        SessionListView()
+            .onAppear {
+                try! dbQueue.write { db in
+                    for _ in 0..<5 {
+                        let startTime = Float(Date.now.timeIntervalSince1970)
+                        let endTime = Float(Date.now.addingTimeInterval(342).timeIntervalSince1970)
+                        
+                        try! SessionModel(id: UUID(), startTimestamp: startTime, endTimestamp: endTime).insert(db)
+                    }
+                }
+            }
     }
 }
 
 #Preview {
-    SessionListView()
+    PreviewView()
+        .environment(\.databaseQueue, try! .createTemporaryDBQueue())
 }
