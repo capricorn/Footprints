3d8846de0c5b49b6fbdef7ed3b9d3df2359d74ca
capricorn <8250820+capricorn@users.noreply.github.com>
2024-04-10 15:23:29 -0600

    Move navigation to a tab view

--- Footprints/ContentView.swift
+++ Footprints/ContentView.swift
@@ -20,42 +20,16 @@ struct ContentView: View {
     }
     
     var body: some View {
-        // TODO: NavigationSplitView?
-        // Setup a navigation link
-        NavigationSplitView(sidebar: {
-            // **TODO: Implement as list**
-            //Text("test")
-            ForEach(Navigation.allCases) { selection in
-                //Text("\(selection.rawValue)")
-                NavigationLink(selection.rawValue, destination: {
-                    //Text("\(selection.rawValue)")
-                    switch selection {
-                    case .logger:
-                        LoggerView()
-                    case .sessions:
-                        SessionListView()
-                    }
-                })
-            }
-        }, detail: {
-            // TODO: Handle navigation here
-            Text("Placeholder")
-            //LoggerView()
-            //switch
-        })
-        /*
-        NavigationStack {
-            // Does this require specifying a separate navigation pane?
-            NavigationLink(destination: {
-                LoggerView()
-            }, label: { Text("Test")})
-            /*
-            Text("Test1")
-                .navigatio
-             */
-            
+        TabView {
+            LoggerView()
+                .tabItem {
+                    Label("Logger", systemImage: "record.circle")
+                }
+            SessionListView()
+                .tabItem {
+                    Label("Sessions", systemImage: "list.bullet.rectangle")
+                }
         }
-         */
     }
 }
 
