e121146b54b6e3cf5ce0b0bdad5bb4dd0c2db65e
capricorn <8250820+capricorn@users.noreply.github.com>
2024-04-16 14:27:22 -0600

    Add skeleton for icon rendering

---
+++ Footprints/FootprintsIcon.swift
@@ -0,0 +1,20 @@
+//
+//  FootprintsIcon.swift
+//  Footprints
+//
+//  Created by Collin Palmer on 4/16/24.
+//
+
+import SwiftUI
+
+struct FootprintsIcon: View {
+    var body: some View {
+        // TODO: Draw circle (presumably linear) -- smallest is 1/4 of largest?
+        // (Try to make similar square as that of app icon -- maybe a custom view for this to snapshot..?)
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    FootprintsIcon()
+}
