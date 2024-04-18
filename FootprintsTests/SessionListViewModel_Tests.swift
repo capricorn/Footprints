604b122436815e6d504286f27beaf796ced42b03
capricorn <8250820+capricorn@users.noreply.github.com>
2024-04-18 15:30:20 -0600

    Setup session list vm tests

---
+++ FootprintsTests/SessionListViewModel_Tests.swift
@@ -0,0 +1,18 @@
+//
+//  SessionListViewModel_Tests.swift
+//  FootprintsTests
+//
+//  Created by Collin Palmer on 4/18/24.
+//
+
+import XCTest
+
+final class SessionListViewModel_Tests: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
