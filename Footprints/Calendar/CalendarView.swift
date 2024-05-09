578b73ac7d9b8e616a902396e0fcd61d8e074f02
capricorn <8250820+capricorn@users.noreply.github.com>
2024-05-09 16:17:13 -0600

    Rough initial implementation of calendar record view

---
+++ Footprints/Calendar/CalendarView.swift
@@ -0,0 +1,104 @@
+//
+//  CalendarView.swift
+//  Footprints
+//
+//  Created by Collin Palmer on 5/9/24.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    private let squareSide: CGFloat = 16
+    let today: Date
+    let daysInMonth: Int
+    let daysInLastWeek: Int
+    let selectedDates: [Date]
+    private let dateMap: Set<Int>// = Set()
+    
+    // Assumption: all dates passed are within this month (maybe assert?)
+    init(_ selectedDates: [Date]=[]) {
+        self.today = Date.now
+        self.daysInMonth = Calendar.current.range(of: .day, in: .month, for: self.today)!.upperBound
+        self.daysInLastWeek = max(self.daysInMonth - 28, 0)
+        self.selectedDates = selectedDates
+        //self.dateMap.insert(1)
+        
+        let setDays = self.selectedDates.map {
+            return Calendar.current.dateComponents([.day], from: $0).day!
+        }
+        
+        self.dateMap = Set(setDays)
+        print("Date map: \(self.dateMap)")
+    }
+    
+    // TODO: Separate view where you can set color
+    var calendarSquare: some View {
+        RoundedRectangle(cornerRadius: 4)
+            .fill(.blue)
+            .frame(width: squareSide, height: squareSide)
+    }
+    
+    var emptyCalendarSquare: some View {
+        RoundedRectangle(cornerRadius: 4)
+            .stroke(.gray)
+            .frame(width: squareSide, height: squareSide)
+    }
+    
+    var placeholderCalendarSquare: some View {
+        Rectangle()
+            .fill(.white)
+            .frame(width: squareSide, height: squareSide)
+    }
+    
+    func dayOfMonth(week: Int, day: Int) -> Int {
+        return week*7 + day
+    }
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<5, id: \.self) { (week: Int) in
+                HStack {
+                    if week == 4 {
+                        ForEach(0..<self.daysInLastWeek, id: \.self) { day in
+                            // Check if a date with this day of the week exists
+                            //if inMonth(calendarDate(week: week, day: day)) {
+                            if self.dateMap.contains(dayOfMonth(week: week, day: day)) {
+                                calendarSquare
+                            } else {
+                                emptyCalendarSquare
+                            }
+                        }
+                        ForEach(0..<(7-self.daysInLastWeek), id: \.self) { _ in
+                            // TODO: Should use background depending on dark mode or not
+                            placeholderCalendarSquare
+                        }
+                    } else {
+                        ForEach(0..<7, id: \.self) { day in
+                            if self.dateMap.contains(dayOfMonth(week: week, day: day)) {
+                                calendarSquare
+                            } else {
+                                emptyCalendarSquare
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension Date {
+    // Alternative: offset(.days(3))
+    static func now(offsetByDays days: Int) -> Date {
+        return Date.now.addingTimeInterval(60*60*24*Double(days))
+    }
+}
+
+#Preview {
+    CalendarView([
+        .now,
+        .now(offsetByDays: 4),
+        .now(offsetByDays: 6),
+        .now(offsetByDays: -8)
+    ])
+}
